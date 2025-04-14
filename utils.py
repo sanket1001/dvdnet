aa8ac8ef95bb93ec72e24357cd96eaf478a6016f
Sanket Badgujar <sanketbadgujar1001@gmail.com>
2025-04-14 17:38:14 -0500

    Update utils.py updated skimage.metrics
    remove compare_psnr 
    add peak_signal_noise_ratio

--- utils.py
+++ utils.py
@@ -19,7 +19,7 @@ from random import choices # requires Python >= 3.6
 import numpy as np
 import cv2
 import torch
-from skimage.measure.simple_metrics import compare_psnr
+from skimage.metrics import peak_signal_noise_ratio
 
 IMAGETYPES = ('*.bmp', '*.png', '*.jpg', '*.jpeg', '*.tif') # Supported image types
 
@@ -143,8 +143,7 @@ def batch_psnr(img, imclean, data_range):
 	imgclean = imclean.data.cpu().numpy().astype(np.float32)
 	psnr = 0
 	for i in range(img_cpu.shape[0]):
-		psnr += compare_psnr(imgclean[i, :, :, :], img_cpu[i, :, :, :], \
-					   data_range=data_range)
+		psnr += peak_signal_noise_ratio(imgclean[i, :, :, :], img_cpu[i, :, :, :], data_range=data_range)
 	return psnr/img_cpu.shape[0]
 
 def augment_train_pair(inarr):
